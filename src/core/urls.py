b71f2f1e39e2df076b164ffcc874110bc03884a0
Marcus Gabaldo <gabaldo@gmail.com>
2018-11-27 19:01:11 -0200

    first core template

--- src/core/urls.py
+++ src/core/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
-
+from .views import HomeView
 
 urlpatterns = [
-	path('', include('src.core.urls')),
-    path('admin/', admin.site.urls),
+	path('', HomeView.as_view(), name='home')
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+class HomeView(View):
+	template_name = 'core/home.html'
+	# GET method
+	def get(self, request, *args, **kwargs):
+		return render(request, self.template_name, {})
