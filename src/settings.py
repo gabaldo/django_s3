7efea27a50f8ab59389816838ed80c043f24d161
Marcus Gabaldo <gabaldo@gmail.com>
2018-11-27 21:58:16 -0200

    create aws s3 connection

--- src/settings.py
+++ src/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 from decouple import config
 from dj_database_url import parse as dburl
+from src.aws.conf import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'src.core',
+    'storages',
 ]
 
 MIDDLEWARE = [
