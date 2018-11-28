f8a6ade850c6133aef3993c921bba561ef719f6d
Marcus Gabaldo <gabaldo@gmail.com>
2018-11-27 23:05:20 -0200

    change to boto

--- src/aws/utils.py
+++ src/aws/utils.py
@@ -1,4 +1,11 @@
 from storages.backends.s3boto3 import S3Boto3Storage
+from storages.backends.s3boto import S3BotoStorage
 
-StaticRootS3BotoStorage = lambda: S3Boto3Storage(location='static')
-MediaRootS3BotoStorage  = lambda: S3Boto3Storage(location='media')
+# Utilizando o Boto3
+# Por motivos de instabilidade, configurar os controles de acesso ACLs como 'falso' no aws s3
+# StaticRootS3BotoStorage = lambda: S3Boto3Storage(location='static')
+# MediaRootS3BotoStorage  = lambda: S3Boto3Storage(location='media')
+
+# Utilizando o Boto
+StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
+MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='media')
